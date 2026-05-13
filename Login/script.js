5b9b038c0d14dd72c978a24b4a1b9fc8f30e0cd3
DavidGz <davidgz09y@gmail.com>
2026-05-12 22:14:53 -0500

    js funcional (primera parte)

--- Login/script.js
+++ Login/script.js
@@ -5,14 +5,14 @@ togglePassword.addEventListener('click', function () {
     // Cambia el tipo: si es password lo pasa a text y viceversa
     const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordField.setAttribute('type', type);
-    
+
     // Cambia el icono: de ojo abierto a ojo tachado
     this.querySelector('i').classList.toggle('bi-eye');
     this.querySelector('i').classList.toggle('bi-eye-slash');
 });
 
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const cargo = document.getElementById('cargo').value;
@@ -20,14 +20,28 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const mensajeError = document.getElementById('mensajeError');
     const passInput = document.getElementById('password');
 
-    if (password === "12345678") {
+    if (
+        (cargo === "Administrador" && password === "admin123") ||
+        (cargo === "Gerente" && password === "gerente123") ||
+        (cargo === "Operador" && password === "operador123")
+    ) {
+
         mensajeError.classList.add('d-none');
         passInput.classList.remove('is-invalid');
+
         alert("Acceso concedido para: " + cargo);
-        window.location.href = ""
+
+        setTimeout(() => {
+            window.location.href = "../MainMenu/mainMenu.html";
+        }, 1500);
+
     } else {
+
         mensajeError.classList.remove('d-none');
         passInput.classList.add('is-invalid');
+
     }
 });
+
+
 
